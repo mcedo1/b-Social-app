b687c43fd3493d3c8444df438877d25c212a8d69
Slavko Sosic <sosicslavko8@gmail.com>
2024-01-30 12:14:10 +0100

    First part of app is done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/footer.component/Footer';
+import Login from './components/log_reg.component/Login';
+import Registration from './components/log_reg.component/Registration';
+import MainPage from './components/main.pagecomponent/MainPage';
+
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App'>
+      <div className="Main">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/reg" element={<Registration />} />
+          <Route path="/main" element={<MainPage/>} />
+        </Routes>
+      </div>
+      <Footer/>
+     </div>
   );
 }
 
---
+++ src/components/main.pagecomponent/MainPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainPage() {
+  return (
+    <div>main.page</div>
+  )
+}
+
+export default MainPage
